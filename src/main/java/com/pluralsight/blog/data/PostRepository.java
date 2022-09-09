6d10fdf6903bc45ffd79d3afc2b662ff2ef4c07a
spryntyB <82833049+spryntyB@users.noreply.github.com>
2022-09-09 14:32:41 +0200

    unit1

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,10 +1,15 @@
 package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
 
+
+public interface PostRepository extends JpaRepository<Post, Long> {}
+
+/*
 @Component
 public class PostRepository {
 
@@ -51,3 +56,5 @@ public class PostRepository {
         return Optional.empty();
     }
 }
+
+*/
